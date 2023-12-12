caa171432fe2b275a9dbd9c452d8d81b73dd5507
da0208 <diotvmwquorfck2938561038@gmail.com>
2023-12-13 02:11:39 +0900

    Attendance

--- main.py
+++ main.py
@@ -4,11 +4,11 @@ import sqlite3
 
 app = Flask(__name__)
 
-# データベースに接続
+
 conn = sqlite3.connect('attendance.db', check_same_thread=False)
 c = conn.cursor()
 
-# 勤怠テーブルを作成（すでに存在する場合は何もしない）
+
 c.execute('''
     CREATE TABLE IF NOT EXISTS attendance (
         name TEXT,
@@ -21,26 +21,26 @@ c.execute('''
 def index():
     if request.method == 'POST':
         if 'submit' in request.form:
-            # 現在の日時を取得
+          
             now = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
 
-            # 勤怠データをデータベースに保存
+            
             c.execute('INSERT INTO attendance VALUES (?, ?, ?)', (request.form['name'], now, request.form['type']))
 
-            # 変更をコミット
+            
             conn.commit()
 
             return f'{request.form["name"]} {request.form["type"]} at {now}'
         elif 'delete' in request.form:
-            # 勤怠データをデータベースから削除
+            
             c.execute('DELETE FROM attendance WHERE name = ?', (request.form['name'],))
 
-            # 変更をコミット
+            
             conn.commit()
 
             return f'{request.form["name"]} data deleted.'
     else:
-        # 勤怠データをデータベースから取得
+        
         c.execute('SELECT * FROM attendance')
         data = c.fetchall()
 
